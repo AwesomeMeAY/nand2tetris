567692624812302d1621b4666ccbba224f07466a
Zeitiah <awesomemeandawesomeyou@gmail.com>
2021-02-17 11:56:54 -0600

    Wed Feb 17 11:56:54 AM CST 2021

--- projects/06/assembler/HackAssembler.py
+++ projects/06/assembler/HackAssembler.py
@@ -2,13 +2,13 @@
 import sys
 from convert_decimal import convert_to_binary
 
-## Returns a list of the program without useless lines
+# Returns a list of the program without useless lines
 def clean_program(program):
-    ## Get rid of comments and empty lines
+    # Get rid of comments and empty lines
     return_lst = []
     for line in program:
         if line != '\n':
-            ## If '//' in line then get rid of everything after it 
+            # If '//' in line then get rid of everything after it 
             try:
                 comment_less_line = line[:line.index('//')]
                 if comment_less_line != '':
@@ -21,62 +21,55 @@ def parser(program):
     for program_line in program:
         if '(' in program_line:
             continue
-        ## If A instruction: 
+        # If A instruction: 
         if program_line[0] == '@':
             output_program.writelines([convert_to_hack(instruction_type=0, address=program_line[1:]), '\n'])
             continue
-        ## If C instruction:
-        ## Splits instruction into dest, comp, and jump
+        # If C instruction:
+        # Splits instruction into dest, comp, and jump
         program_line = program_line.replace('=', ' ').replace(';', ' ').split()
-        
         try:
-            ## Need to remove comp because comp and dest can have same values
+            # Need to remove comp because comp and dest can have same values
             if program_line[1] not in binary_jumps:
                 comp = program_line.pop(1)
             else:
                 comp = program_line.pop(0)
-            ## If any item in program_line is in binary_destinations:
-            if any(i in binary_destinations for i in program_line):
-                ## If dest in program_line then it will be at index 0
-                dest = program_line[0]
+            if program_line[0] in binary_destinations:
+                dest = program_line.pop(0)
             else:
                 dest = 'null'
-            ## Have to do it the bloat way because jump could be at index 0 or 1
-            for i in program_line:
-                if i in binary_jumps:
-                    jump = i
-                    break
+            if program_line:
+                jump = program_line.pop(0)
             else:
                 jump = 'null'
             output_program.writelines([convert_to_hack(comp=comp, dest=dest, jump=jump), '\n'])
-            continue
-        ## If program_line has only one item in it then it will be comp
+        # If program_line has only one item in it then it will be comp
         except IndexError:
+            print('One singular computation, no jumps or anything')
             output_program.writelines([convert_to_hack(comp=program_line[0], dest='null', jump='null'), '\n'])
-            continue
         
     
 
-## Defaults to C instruction 
-## If an argument is -1 it is not used
+# Defaults to C instruction 
+# If an argument is -1 it is not used
 def convert_to_hack(instruction_type=1, address=-1, comp=-1, dest=-1, jump=-1):
     if instruction_type:
         return f'{instruction_type}11{binary_instructions[comp]}{binary_destinations[dest]}{binary_jumps[jump]}'
-    ## If A instruction 
-    ## Check if it is a know symbol
+    # If A instruction 
+    # Check if it is a know symbol
     if address in symbols:
         return f'{instruction_type}{symbols[address]}'
-    ## Check if it is an integer or an unknown symbol
+    # Check if it is an integer or an unknown symbol
     try:
         machine_code = f'{instruction_type}{convert_to_binary(int(address))[1:]}'
         return machine_code
-    ## If unknown symbol:
+    # If unknown symbol:
     except ValueError:
         used_registers.append(used_registers[-1] + 1)
         symbols[address] = convert_to_binary(used_registers[-1])[1:]
         return f'{instruction_type}{symbols[address]}'
 
-## line_number should be the next Line Number in binary 
+# line_number should be the next Line Number in binary 
 def label_handler(program_line, line_number):
     if program_line[0] == '(':
         symbols[program_line[1:program_line.index(')')]] = line_number
@@ -85,7 +78,7 @@ def label_handler(program_line, line_number):
 
 if __name__ == '__main__':
     assembly_program = open(sys.argv[1]).readlines()
-    output_program = open(f"{sys.argv[1].replace('.asm', '')}.hack", 'w')
+    output_program = open(f"{sys.argv[1].replace('.asm', '.hack')}", 'w')
 
     # Setting symbols
     symbols = {'SCREEN':convert_to_binary(16384)[1:], 'KBD':convert_to_binary(24576)[1:]}
@@ -94,7 +87,7 @@ if __name__ == '__main__':
 
     used_registers = [15]
 
-    ## Searching a dictionary is faster than a conditionals
+    # Searching a dictionary is faster than a conditionals
     binary_instructions = {'0':'0101010', '1':'0111111', '-1':'0111010', 'D':'0001100', 'A':'0110000',
                            '!D':'0001101', '!A':'0110001', '-D':'0001111', '-A':'0110011', 'D+1':'0011111',
                            'A+1':'0110111', 'D-1':'0001110', 'A-1':'0110010', 'D+A':'0000010', 'D-A':'0010011',
@@ -106,12 +99,12 @@ if __name__ == '__main__':
 
     binary_jumps = {'null':'000', 'JGT':'001', 'JEQ':'010', 'JGE':'011', 'JLT':'100', 'JNE':'101', 'JLE':'110', 'JMP':'111'}
 
-    ## Main():
-   
-    line_number = 0
+    # Main():
+    line_number = 0 
     cleaned_program = clean_program(assembly_program)
     for line in cleaned_program:
-        label_handler(line, convert_to_binary(line_number)[1:])
+        if label_handler(line, convert_to_binary(line_number)[1:]):
+            continue
         line_number += 1
     parser(cleaned_program)
         
